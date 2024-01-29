65ecaf72d0548448c895772a8791628e26df9f59
Alexander Köpke <koepalex@xdigital.de>
2024-01-29 22:58:59 +0100

    Added GitHub CLI

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -47,3 +47,10 @@ rm packages-microsoft-prod.deb
 sudo apt update
 sudo apt install -y dotnet-sdk-7.0
 
+echo "GitHub CLI installing..."
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
