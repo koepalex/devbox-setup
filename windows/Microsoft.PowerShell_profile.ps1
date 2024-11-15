5fb7d93f834f46b65b73be913311e2d6aa60022d
Alexander Koepke <alexander.koepke@microsoft.com>
2024-11-15 13:44:24 +0100

    add command rdev to execute cargo watch test

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,8 @@
 Import-Module -Name Terminal-Icons
 Set-Alias code code-insiders
+Set-Alias rdev launch_cargo_watch
+
+Function launch_cargo_watch { cargo watch -c -w src -x test }
 
 oh-my-posh --init --shell pwsh --config ".\nu4a.omp.json" | Invoke-Expression
 
